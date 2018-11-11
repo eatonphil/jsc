d803bbc2ad5c8eb75c0cc346b41b2cb3f92f58bc
Phil Eaton <phil@capsule8.com>
2018-11-10 22:46:12 -0500

    Add support for let/const (#8)
    * Add support for let/const
    
    * Add const test
    
    * Fix indentation
    
    * Drop const tests

--- src/jsc/parser.rs
+++ src/jsc/parser.rs
@@ -5,10 +5,10 @@ use std::string::String;
 extern crate easter;
 extern crate esprit;
 
-pub fn from_file(the_file: &str) -> easter::prog::Script {
+pub fn from_file(the_file: &str) -> easter::stmt::Script {
     let mut f = File::open(the_file)
         .expect(format!("{} not found", the_file).as_str());
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("error reading input");
-    esprit::script(contents.as_str()).expect("error parsing input")
+    esprit::strict(contents.as_str()).expect("error parsing input")
 }
